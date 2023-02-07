83b1d8a1af61162008a730c60ac5ef4e43ba0d50
AshutoshDash1999 <dashashutosh1999@gmail.com>
2023-02-07 09:15:31 +0530

    first commit🎉

---
+++ components/EventCard.js
@@ -0,0 +1,63 @@
+import { Box, Button, Flex, Grid, Text, Title } from "@mantine/core";
+import Link from "next/link";
+
+const EventCard = () => {
+  return (
+    <Box
+      sx={(theme) => ({
+        backgroundColor:
+          theme.colorScheme === "dark"
+            ? theme.colors.dark[6]
+            : theme.colors.gray[0],
+        textAlign: "center",
+        padding: theme.spacing.sm,
+        margin: theme.spacing.xl,
+        borderRadius: theme.radius.md,
+        cursor: "default",
+      })}
+    >
+      <Grid>
+        <Grid.Col span={2}>
+          <Box
+            sx={(theme) => ({
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[6]
+                  : theme.colors.orange[3],
+              textAlign: "center",
+              padding: theme.spacing.sm,
+              borderRadius: theme.radius.md,
+              cursor: "default",
+            })}
+          >
+            <Text>05 Feb - 08 Feb</Text>
+            <Text>10:00AM</Text>
+            <Text>IST</Text>
+          </Box>
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <Box>
+            <Title>Competition Name</Title>
+            <Text>start time to end time</Text>
+            <Text>duration</Text>
+          </Box>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Flex direction="column" gap="md">
+            <Link href="#" target="_blank">
+              <Button variant="filled" color="dark">
+                Visit Competition
+              </Button>
+            </Link>
+            <Link href="#" target="_blank">
+              <Button variant="subtle" color="dark">
+                Add to Calender
+              </Button>
+            </Link>
+          </Flex>
+        </Grid.Col>
+      </Grid>
+    </Box>
+  );
+};
+export default EventCard;
---
+++ components/EventList.js
@@ -0,0 +1,10 @@
+import EventCard from "./EventCard"
+
+const EventList = () => {
+  return (
+    <div>
+      <EventCard/>
+    </div>
+  )
+}
+export default EventList
