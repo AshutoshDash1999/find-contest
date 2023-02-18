d807aeb4f3722e8d46e3e412da712219b10d2d78
AshutoshDash1999 <dashashutosh1999@gmail.com>
2023-02-18 23:14:08 +0530

    feat: dynamically fetch supported sites✨

--- components/FilterSection.js
+++ components/FilterSection.js
@@ -1,21 +1,70 @@
 import { Container, Flex, Input, Paper, Select, Switch } from "@mantine/core";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const FilterSection = () => {
+  const [filterMethods, setFilterMethods] = useState({
+    searchKeyword: "",
+    plateform: "all",
+    ongoing: true,
+  });
+
+  const [supportedSiteDataList, setSupportedSiteDataList] = useState([]);
+
+  const sitesApiUrl = "https://kontests.net/api/v1/sites";
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      // fetch data to show all the supported site
+      try {
+        const supportedSiteArray = [];
+        const responseData = await axios
+          .get(sitesApiUrl)
+          .then((response) =>
+            response.data.map((siteItem) =>
+              supportedSiteArray.push(siteItem[0])
+            )
+          )
+          .catch((error) =>
+            console.error("Error while fetching data: ", error)
+          );
+        setSupportedSiteDataList(supportedSiteArray);
+      } catch (error) {
+        console.error("Error while fetching data: ", error);
+      }
+    };
+    fetchAPI();
+  }, []);
+
   return (
     <Container>
       <Paper shadow="sm" radius="lg" p="md">
-        <Flex align="center" justify="center" gap="xl">
-          <Input placeholder="Search competition..." radius="md" w="100%" />
+        <Flex
+          direction={{ base: "column", sm: "row" }}
+          align="center"
+          justify="center"
+          gap="xl"
+        >
+          <Input
+            placeholder="Search competition..."
+            radius="md"
+            w="100%"
+            onChange={(e) => console.log(e.target.value)}
+          />
 
           <Select
             placeholder="Pick platform"
             searchable
-            nothingFound="No options"
+            nothingFound="Not found!"
             radius="md"
-            data={["Leetcode", "Hackerrank", "Codeforces", "Codechef"]}
+            data={supportedSiteDataList}
             w="100%"
+            onChange={(e) => console.log(e)}
           />
-          <Switch label="Ongoing" />
+          <Switch
+            label="Ongoing"
+            onChange={(e) => console.log(e.target.checked)}
+          />
         </Flex>
       </Paper>
     </Container>
